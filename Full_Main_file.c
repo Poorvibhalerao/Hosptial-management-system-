fbbb8a7b996252b956fe63ee4383b62e318dfd49
Poorvibhalerao <152211521+Poorvibhalerao@users.noreply.github.com>
2023-12-01 21:13:01 +0530

    Update Full_Main_file.c

--- Full_Main_file.c
+++ Full_Main_file.c
@@ -166,4 +166,18 @@ void newappointment()
     }
     fclose(fp);
 }
+void delete(){
 
+char buff[20];
+printf("enter the patient file name that you want to delete\n\n");
+scanf("%s",buff);
+
+if(remove(buff)==0){
+    printf("the file is deleted successfully!!\n\n");
+}
+else{
+    printf("There is no record available for this person\n\n");
+}
+
+
+}
